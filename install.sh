bb2dc4c7b01b9c61ba45f87ae7f6ad1e55776615
Wafelack <wafelack@riseup.net>
2021-09-08 16:48:53 +0200

    fix: install path

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 PROGRAM=xbps-freedom
 PREFIX=/usr
 install -m 644 -o root -g root -t ${PREFIX}/share/man/man1/ ${PROGRAM}.1
-install -m 755 -o root -g root -t ${PREFIX}/bin/ ${PROGRAM}.pl
+install -m 755 -o root -g root -T ${PROGRAM}.pl ${PREFIX}/bin/${PROGRAM}
